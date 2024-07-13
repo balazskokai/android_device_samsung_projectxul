ec60cd494dbc99ba17aebd2bae47d9463a270b4c
Kokai Balazs <balazs.kokai.alt@gmail.com>
2024-07-13 17:50:59 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_projectxul-user
-add_lunch_combo omni_projectxul-userdebug
-add_lunch_combo omni_projectxul-eng
+add_lunch_combo twrp_projectxul-user
+add_lunch_combo twrp_projectxul-userdebug
+add_lunch_combo twrp_projectxul-eng
